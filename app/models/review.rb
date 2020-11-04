fe7cc7ad4f8ad263f7e1803c8ea2cf337885e4ad
Jessica Ho <hwyann@gmail.com>
2020-11-04 13:05:42 +0900

    restaurant done

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  RATING = (0..5)
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: RATING }, numericality: { only_integer: true }
+end
